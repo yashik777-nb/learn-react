5d0bc5c80af0f72fa142ff1316950a950ab90ea1
Yash <yaswanth.iddibani@gmail.com>
2021-01-23 22:09:13 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from "./logo.svg";
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends React.Component {
+  state = {
     persons: [
       {
         name: "Yash",
@@ -19,95 +20,66 @@ const App = (props) => {
         age: 32,
       },
     ],
-  });
+  };
 
-  const [counterState, setCounerState] = useState({ counter: 0 });
-
-  console.log(personsState);
-  console.log(counterState);
-
-  const increment = () => {
-    setPersonsState({
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
         {
-          name: "Yashik",
+          name: newName,
           age: 12,
         },
+        {
+          name: "SaiK",
+          age: 22,
+        },
+        { name: "SamJ", age: 33 },
       ],
-    });
-    setCounerState({
-      counter: counterState.counter + 1,
     });
   };
 
-  return (
-    <div className="App">
-      <h1> I am a React App</h1>
-      <p>This is Working</p>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      >
-        Using State
-      </Person>
-      <p>Counter: {counterState.counter}</p>
-      <button onClick={increment}>My Button</button>
-      <Person name="Sai" age="21" />
-      <Person name="Sam" age="32" />
-    </div>
-    // return React.createElement('div', null, React.createElement('h1', {className: 'App', "Second Componet"}))
-  );
-};
+  nameChangedHandler = (event) => {
+    this.state({
+      persons: [
+        {
+          name: "Yash",
+          age: 12,
+        },
+        {
+          name: "Hello",
+          age: 22,
+        },
+        { name: "SamJ", age: 33 },
+      ],
+    });
+  };
 
-// class App extends React.Component {
-//   state = {
-//     persons: [
-//       {
-//         name: "Yash",
-//         age: 28,
-//       },
-//       {
-//         name: "Sai",
-//         age: 21,
-//       },
-//       {
-//         name: "Sam",
-//         age: 32,
-//       },
-//     ],
-//     counter: 0,
-//   };
-
-//   increment = () => {
-//     this.setState({
-//       persons: [
-//         {
-//           name: "Yashik",
-//           age: 12,
-//         },
-//       ],
-//       counter: this.state.counter + 1,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1> I am a React App</h1>
-//         <p>This is Working</p>
-//         <Person
-//           name={this.state.persons[0].name}
-//           age={this.state.persons[0].age}
-//         >
-//           Using State
-//         </Person>
-//         <p>Counter: {this.state.counter}</p>
-//         <button onClick={this.increment}>My Button</button>
-//         <Person name="Sai" age="21" />
-//         <Person name="Sam" age="32" />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div className="App">
+        <h1> I am a React App</h1>
+        <p>This is Working</p>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+        >
+          Using State
+        </Person>
+        <button onClick={this.switchNameHandler.bind(this, "YashIK Bind")}>
+          My Button
+        </button>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={() => this.switchNameHandler("Sai Uk")}
+          changed={this.nameChangedHandler}
+        />
+        <Person name="Sam" age="32" />
+        <Person />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,15 @@
 export default function Person(props) {
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         I am {props.name}. I am {props.age} years Old. {props.children}
       </p>
+      <input type="text" onChange={props.changed}></input>
     </div>
   );
 }
+
+Person.defaultProps = {
+  name: "Default",
+  age: 0,
+};
