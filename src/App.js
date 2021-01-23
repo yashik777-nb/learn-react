003f00cf429a9e0313052f4f9e5f90a008d497ce
Yash <yaswanth.iddibani@gmail.com>
2021-01-23 11:45:46 +0000

    State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,113 @@
 // import logo from "./logo.svg";
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {
+        name: "Yash",
+        age: 28,
+      },
+      {
+        name: "Sai",
+        age: 21,
+      },
+      {
+        name: "Sam",
+        age: 32,
+      },
+    ],
+  });
+
+  const [counterState, setCounerState] = useState({ counter: 0 });
+
+  console.log(personsState);
+  console.log(counterState);
+
+  const increment = () => {
+    setPersonsState({
+      persons: [
+        {
+          name: "Yashik",
+          age: 12,
+        },
+      ],
+    });
+    setCounerState({
+      counter: counterState.counter + 1,
+    });
+  };
+
   return (
     <div className="App">
       <h1> I am a React App</h1>
       <p>This is Working</p>
-      <Person name="Yash">My Hobbies: Learning </Person>
-      <Person name="Sai" />
-      <Person />
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      >
+        Using State
+      </Person>
+      <p>Counter: {counterState.counter}</p>
+      <button onClick={increment}>My Button</button>
+      <Person name="Sai" age="21" />
+      <Person name="Sam" age="32" />
     </div>
     // return React.createElement('div', null, React.createElement('h1', {className: 'App', "Second Componet"}))
   );
-}
+};
+
+// class App extends React.Component {
+//   state = {
+//     persons: [
+//       {
+//         name: "Yash",
+//         age: 28,
+//       },
+//       {
+//         name: "Sai",
+//         age: 21,
+//       },
+//       {
+//         name: "Sam",
+//         age: 32,
+//       },
+//     ],
+//     counter: 0,
+//   };
+
+//   increment = () => {
+//     this.setState({
+//       persons: [
+//         {
+//           name: "Yashik",
+//           age: 12,
+//         },
+//       ],
+//       counter: this.state.counter + 1,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1> I am a React App</h1>
+//         <p>This is Working</p>
+//         <Person
+//           name={this.state.persons[0].name}
+//           age={this.state.persons[0].age}
+//         >
+//           Using State
+//         </Person>
+//         <p>Counter: {this.state.counter}</p>
+//         <button onClick={this.increment}>My Button</button>
+//         <Person name="Sai" age="21" />
+//         <Person name="Sam" age="32" />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,7 @@ export default function Person(props) {
   return (
     <div>
       <p>
-        I am {props.name}. I am {Math.floor(Math.random() * 30)} years Old.{" "}
-        {props.children}
+        I am {props.name}. I am {props.age} years Old. {props.children}
       </p>
     </div>
   );
